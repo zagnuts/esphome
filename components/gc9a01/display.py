c9b24b2cf6f3b67113c0eb95cbe9ada0820ec71e
micahed <github@lrb.me.uk>
2024-01-17 15:00:23 +0000

    Update display.py

--- components/gc9a01/display.py
+++ components/gc9a01/display.py
@@ -53,7 +53,7 @@ CONFIG_SCHEMA = cv.All(
 
 
 async def setup_gc9a01(var, config):
-   if cv.Version.parse(ESPHOME_VERSION) < cv.Version.parse("2023.12.0"):
+    if cv.Version.parse(ESPHOME_VERSION) < cv.Version.parse("2023.12.0"):
         await cg.register_component(var, config)
     await display.register_display(var, config)
 
